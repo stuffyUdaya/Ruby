06947e1053fcf54bdd40f985ad1fc6f26107b1c1
Udaya Tummala <udayatummala1@gmail.com>
2017-03-06 14:50:51 -0500

    first commit in ruby

---
+++ ruby_basics/add.rb
@@ -0,0 +1,18 @@
+x =60; y= 45; z = x+y;
+puts "sum is #{z} "
+
+puts "sum is ", +z
+
+puts "sum is %d" %z
+
+name = "udaya"
+
+puts "This is %s"%name
+
+puts "\t\tThis\n is \t%s"%name
+
+
+a ="this"
+b = " is"
+
+puts a+b
---
+++ ruby_basics/puts.rb
@@ -0,0 +1,19 @@
+puts "Hello"
+puts "welcome"
+puts "Ninja"
+
+print "Hello"
+print "Coding"
+print "Ninja"
+
+# comment
+# executing ruby files: ruby {{filename}} in your terminal
+
+BEGIN{
+  puts "this is in the begin block"
+}
+
+
+END{
+  puts "this is in the end last block"
+}
